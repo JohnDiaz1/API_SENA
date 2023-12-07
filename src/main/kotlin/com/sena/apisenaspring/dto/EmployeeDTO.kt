e8fd84c046e6bab18cfedb3aad6a0c744b16f701
John Diaz <nabetes2001@hotmail.com>
2023-12-07 18:34:29 -0500

    Update model for employee

--- src/main/kotlin/com/sena/apisenaspring/dto/EmployeeDTO.kt
+++ src/main/kotlin/com/sena/apisenaspring/dto/EmployeeDTO.kt
@@ -1,7 +1,7 @@
 package com.sena.apisenaspring.dto
 
 data class EmployeeDTO(
-    val employee: String?,
+    val employeeId: String?,
     val name: String,
     val phone: String,
     val position: String,
